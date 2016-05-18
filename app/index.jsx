8d1e85ab58c8fac88115e91fbb84ae616f967201
Tenzin <sinzin91@gmail.com>
2016-05-18 14:44:59 -0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Griddle = require('griddle-react');
+
+import topo from './topo.json';
+//import exampleMetadata from './exampleMetadata.json';
+//import data from './data.json';
+import metadata from './metadata.json';
+import './main.css';
+
+var columns = [
+  "scraperClassName",
+  "Search Terms",
+  "Javascript",
+  "Price Selector",
+  "Marketplace/CSE",
+  "Multiple Variations"
+]
+
+// ReactDOM.render(<Griddle results={data} enableInfiniteScroll={true} bodyHeight={400} showFilter={true}
+//   columnMetadata={exampleMetadata} showFilter={true} showSettings={true} columns={["name", "city", "state", "country"]}/>, document.getElementById('main'));
+
+ReactDOM.render(<Griddle results={topo} enableInfiniteScroll={true} bodyHeight={500} 
+  useFixedHeader={false} useGriddleStyles={true} columnMetadata={metadata} showSettings={true} 
+  columns={columns} showFilter={true}/>, document.getElementById('main'));
